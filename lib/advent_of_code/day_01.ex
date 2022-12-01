5d06e541c0da5eb07e1515344ef49d8048417c31
Brendan Coulson <brendan@headspacetech.com>
2022-12-01 07:30:50 +0000

    Day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,29 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+
+  def calc_elf_pack(elf) do
+    elf
+    |> String.split("\n")
+    |> Enum.map(fn x -> x |> String.to_integer() end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  defp split_into_elves(input) do
+    input |> String.trim() |> String.split("\n\n")
+  end
+
+  def part1(args) do
+    args
+    |> split_into_elves()
+    |> Enum.map(fn x -> x |> AdventOfCode.Day01.calc_elf_pack() end)
+    |> Enum.max()
+  end
+
+  def part2(args) do
+    args
+    |> split_into_elves()
+    |> Enum.map(fn x -> x |> AdventOfCode.Day01.calc_elf_pack() end)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,19 +3,22 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+  test "calc elf pack" do
+    result = calc_elf_pack("1\n2\n3")
+    assert result == 6
   end
 
-  @tag :skip
+  test "part1" do
+    input = AdventOfCode.Input.get!(1,2022)
+    result = part1(input)
+
+    assert result == 69310
+  end
+
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(1,2022)
     result = part2(input)
 
-    assert result
+    assert result == 206104
   end
 end
