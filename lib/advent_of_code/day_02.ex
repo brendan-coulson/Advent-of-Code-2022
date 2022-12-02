a0e339353b9edc2e4e71c44582d702459fcf076b
Brendan Coulson <brendan@headspacetech.com>
2022-12-02 06:30:57 +0000

    day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,13 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+
+
+  def part1(args) do
+    scores = %{"A X" => 4, "A Y" => 8, "A Z" => 3,"B X" => 1, "B Y" => 5, "B Z" => 9, "C X" => 7, "C Y" => 2, "C Z" => 6}
+    args |> String.trim() |> String.split("\n") |> Enum.map(fn x -> scores[x] end) |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    scores = %{"A X" => 3, "A Y" => 4, "A Z" => 8,"B X" => 1, "B Y" => 5, "B Z" => 9, "C X" => 2, "C Y" => 6, "C Z" => 7}
+    args |> String.trim() |> String.split("\n") |> Enum.map(fn x -> scores[x] end) |> Enum.sum()
   end
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,19 +3,18 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
+
   test "part1" do
-    input = nil
+    input = AdventOfCode.Input.get!(2,2022)
     result = part1(input)
 
-    assert result
+    assert result == 10941
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(2,2022)
     result = part2(input)
 
-    assert result
+    assert result == 13071
   end
 end
