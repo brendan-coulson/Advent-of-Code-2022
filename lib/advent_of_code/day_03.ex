8f281d7be478e72cfdd6ca854c2be30e5f679cc3
Brendan Coulson <brendan@headspacetech.com>
2022-12-03 09:09:58 +0000

    day 3

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,50 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+
+
+  def analyze({left, right}) do
+    ul = left |> String.to_charlist() |> MapSet.new()
+    ur = right |> String.to_charlist() |> MapSet.new()
+    [c] = MapSet.intersection(ul,ur) |> MapSet.to_list
+    if c > 96 do
+      c - 96
+    else
+      c - 38
+    end
   end
 
-  def part2(_args) do
+  def part1(args) do
+    args
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn x -> String.split_at(x,div(String.length(x),2)) end)
+    |> Enum.map(fn x -> analyze(x) end)
+    |> Enum.sum()
+  end
+
+  def threes([]), do: []
+  def threes(lst) do
+    [Enum.take(lst,3) | threes(Enum.drop(lst,3))]
+  end
+
+  def badge([a,b,c]) do
+    as = a |> String.to_charlist() |> MapSet.new()
+    bs = b |> String.to_charlist() |> MapSet.new()
+    cs = c |> String.to_charlist() |> MapSet.new()
+
+    [d] = MapSet.intersection(as,bs) |> MapSet.intersection(cs) |> MapSet.to_list()
+    if d > 96 do
+      d - 96
+    else
+      d - 38
+    end
+  end
+
+  def part2(args) do
+    args
+    |> String.trim()
+    |> String.split("\n")
+    |> threes()
+    |> Enum.map(fn x -> badge(x) end)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -3,19 +3,25 @@ defmodule AdventOfCode.Day03Test do
 
   import AdventOfCode.Day03
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+  test "analyze" do
+    assert analyze({"PnJJfVPBcfVnnPnBFF", "cggttrtgCrjDtSjzSS"}) == 3
   end
 
-  @tag :skip
+  test "part1" do
+    input = AdventOfCode.Input.get!(3,2022)
+    result = part1(input)
+
+    assert result == 7875
+  end
+
+  test "badge" do
+    assert badge(["vJrwpWtwJgWrhcsFMMfFFhFp","jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL","PmmdzqPrVvPwwTWBwg"]) == 18
+  end
+
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(3,2022)
     result = part2(input)
 
-    assert result
+    assert result == 2479
   end
 end
