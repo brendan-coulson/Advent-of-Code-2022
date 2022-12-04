c2fe95429f205be457901d2526b2fb5804b25393
Brendan Coulson <brendan@headspacetech.com>
2022-12-04 05:54:37 +0000

    day 4

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,48 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+
+  def parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn x -> parse_line(x) end)
+  end
+  def parse_line(line) do
+    [left,right | _] = String.split(line,",")
+    [parse_assignment(left),parse_assignment(right)]
+  end
+  def parse_assignment(assign) do
+    assign
+    |> String.split("-")
+    |> Enum.map(fn x -> String.to_integer(x) end)
   end
 
-  def part2(_args) do
+  def analyse1([[la,lb],[ra,rb]]) do
+    cond do
+      la >= ra and lb <= rb -> 1
+      ra >= la and rb <= lb -> 1
+      true -> 0
+    end
+  end
+
+  def analyse2([[la,lb],[ra,rb]]) do
+    cond do
+      la >= ra and la <= rb -> 1
+      lb >= ra and lb <= rb -> 1
+      ra >= la and ra <= lb -> 1
+      rb >= la and rb <= lb -> 1
+      true -> 0
+    end
+  end
+
+  def part1(args) do
+    args
+    |> Enum.map(fn x -> analyse1(x) end)
+    |> Enum.sum()
+  end
+
+  def part2(args) do
+    args
+    |> Enum.map(fn x -> analyse2(x) end)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,19 +3,28 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
 
-    assert result
+
+  test "parse assignment" do
+    assert parse_assignment("61-78") == [61,78]
+    assert String.split("61-78","-") == ["61","78"]
+    assert String.to_integer("61") == 61
+    assert Enum.map(["61","78"],fn x -> String.to_integer(x) end) == [61,78]
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
 
-    assert result
+
+  test "part1" do
+    input = AdventOfCode.Input.get!(4,2022)
+    result = part1(parse(input))
+
+    assert result == 644
+  end
+
+  test "part2" do
+    input = AdventOfCode.Input.get!(4,2022)
+    result = part2(parse(input))
+
+    assert result == 926
   end
 end
