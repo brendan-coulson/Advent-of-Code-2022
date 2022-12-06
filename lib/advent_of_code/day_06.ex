e7f45e0076f1fb76e87eb4e3f8d881e5c1b4d6cd
Brendan Coulson <brendan@headspacetech.com>
2022-12-06 06:26:08 +0000

    day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,18 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+
+  def part1(args), do: part1(String.to_charlist(args),0)
+  def part1([a , b , c , d | rest],count) do
+    cond do
+      a != b and a != c and a != d and b != c and b != d and c != d -> count + 4
+      true -> part1([b,c,d,hd(rest) | tl(rest)],count+1)
+    end
   end
 
-  def part2(_args) do
+  def part2(args), do: part2(String.to_charlist(args),0)
+  def part2(lst,count) do
+    cond do
+      lst |> Enum.take(14) |> MapSet.new() |> MapSet.size() == 14 -> count + 14
+      true -> part2(tl(lst),count+1)
+    end
   end
 end
