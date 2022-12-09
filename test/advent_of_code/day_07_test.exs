c75021cb1065750d9f0a1cd636325a00103988aa
Brendan Coulson <brendan@headspacetech.com>
2022-12-09 10:25:22 +0000

    day 7

--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -3,19 +3,19 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
 
-    assert result
+  test "part1" do
+    input = AdventOfCode.Input.get!(7,2022)
+    result = part1(input)
+    assert result == 1077191
   end
 
-  @tag :skip
+
+
   test "part2" do
-    input = nil
+    input = AdventOfCode.Input.get!(7,2022)
     result = part2(input)
 
-    assert result
+    assert result == 5649896
   end
 end
